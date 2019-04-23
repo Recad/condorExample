0a56a03b7531aad6c61b232eb4a179ce4e60a5fc
recad <dacer-133@hotmail.com>
2019-04-23 02:42:21 -0500

    se suben archivos faltantes

---
+++ in0.r
@@ -0,0 +1,7 @@
+# Cargar el CSV
+n<-read.csv(file="nums.csv",head=TRUE,sep=";")
+# mostrar datos leidos
+n
+# mostrar resumen de datos leidos
+summary(n)
+ 
--- runR.bash
+++ runR.bash
@@ -2,15 +2,21 @@
 # extraer R
 d=$(date +'%G/%m/%d %H:%M:%S')
 h=$(hostname -f)
-echo "${d} inicia extraccion 	R-3.3.0.tar.gz en ${h}"
-tar xfz R-3.3.0.tar.gz
+echo "${d} inicia extraccion r330.tgz en ${h}"
+tar xfz R-power.tar.gz
 d=$(date +'%G/%m/%d %H:%M:%S')
 echo "${d} finaliza extraccion r330.tgz"
-
+$(cp $1 R-power/$1 )
+$(cp nums.csv R-power/nums.csv )
 # Ejecutar R con el archivo indicado
 d=$(date +'%G/%m/%d %H:%M:%S')
-echo "${d} inicia ejecución r con entradas $1"
-export R_HOME="${PWD}/R330"
-R330/bin/R -q -f $1
+echo "${d} inicia ejecuci  n r con entradas $1"
+cd R-power/
+
+export LD_LIBRARY_PATH=`pwd`/lib
+export R_HOME=`pwd`
+export R_LIBS=`pwd`/site-library
+echo $(pwd | ls)
+./R -q --file=$1
 d=$(date +'%G/%m/%d %H:%M:%S')
-echo "${d} finaliza ejecución r con entradas $1"
+echo "${d} finaliza ejecuci  n r con entradas $1"
